c99f645392b6cd42c7f709ef9dbb13eec11e8ac0
Staelp <staelpetit@gmail.com>
2023-03-26 17:57:45 -0400

    Created gitignore

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const app = require("./app");
 const port = 9000 || process.env.PORT;
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose.connect(
-  "mongodb+srv://staelp:killacam18@cluster0.rw2xn.mongodb.net/pickster?retryWrites=true&w=majority",
-  () => {
-    console.log("connected to db");
-  }
-);
+mongoose.connect(process.env.MDBCONNECTSTRING, () => {
+  console.log("connected to db");
+});
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
